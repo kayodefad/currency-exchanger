626fc4702abbf963eb4e76416cf54820d3afe54a
kayodefad <fadojuk@gmail.com>
2023-07-07 23:35:21 +0100

    minor cleanups

--- src/app/modules/main/converter/details/details.component.ts
+++ src/app/modules/main/converter/details/details.component.ts
@@ -17,17 +17,14 @@ export class DetailsComponent {
 
   ngOnInit() {
     this.route.queryParamMap.subscribe((params: ParamMap) => {
-      // Access and handle the query parameters here
       const pair = params.get('pair');
 
       if (pair === 'eur-usd') {
         this.currencyFacade.setCurrency('EUR', 'from');
         this.currencyFacade.setCurrency('USD', 'to');
-        // this.currencyFacade.calculateResult();
       } else if (pair === 'eur-gbp') {
         this.currencyFacade.setCurrency('EUR', 'from');
         this.currencyFacade.setCurrency('GBP', 'to');
-        // this.currencyFacade.calculateResult();
       }
     });
   }
